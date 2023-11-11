1a4cb992298c444277fa044e5b8f3d0e48258081
Ahmed Kheiri <a.o.kheiri@gmail.com>
2023-11-11 23:16:52 +0000

    recent updates

--- Solution.py
+++ Solution.py
@@ -68,7 +68,6 @@ class Solution:
         plt.grid()
         plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
         plt.tight_layout()
-        plt.title(title)
         plt.savefig("plots/"+title)
         plt.show()
 
